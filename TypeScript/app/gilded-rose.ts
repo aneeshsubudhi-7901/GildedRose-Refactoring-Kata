a2cf4bac01f82d7739581e824d29617191f91fbf
aneeshsubudhi-7901 <aneeshs7bd@gmail.com>
2026-01-02 11:28:32 +0530

    refactor(gilded-rose): refactored gilded rose and added conjured items support and tested

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,91 @@ export class Item {
   }
 }
 
+interface ItemService {
+  updateItemQuality(item: Item): void;
+}
+
+class NormalItemService implements ItemService {
+  updateItemQuality(item: Item): void {
+    if (item.quality > 0) {
+      item.quality -= 1;
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 1;
+    }
+  }
+}
+
+class AgedBrieItemService implements ItemService {
+  updateItemQuality(item: Item): void {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+}
+
+class BackstagePassesItemService implements ItemService {
+  updateItemQuality(item: Item): void {
+    if (item.quality < 50) {
+      item.quality += 1;
+      if (item.sellIn < 11) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+      if (item.sellIn < 6) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+}
+
+class ConjuredItemService implements ItemService {
+  updateItemQuality(item: Item): void {
+    if (item.quality > 0) {
+      item.quality = Math.max(0, item.quality - 2);
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality = Math.max(0, item.quality - 2);
+    }
+  }
+}
+
+class SulfurasItemService implements ItemService {
+  updateItemQuality(item: Item): void {
+    return;
+  }
+}
+
+class GildRoseItemServiceFactory {
+  static getItemService(item: Item): ItemService {
+    switch (true) {
+      case item.name.includes("Aged Brie"):
+        return new AgedBrieItemService();
+      case item.name.includes("Backstage passes"):
+        return new BackstagePassesItemService();
+      case item.name.includes("Sulfuras"):
+        return new SulfurasItemService();
+      case item.name.includes("Conjured"):
+        return new ConjuredItemService();
+      default:
+        return new NormalItemService();
+    }
+  }
+}
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -18,50 +103,66 @@ export class GildedRose {
   }
 
   updateQuality() {
+    // for (let i = 0; i < this.items.length; i++) {
+    //   if (
+    //     this.items[i].name != "Aged Brie" &&
+    //     this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+    //   ) {
+    //     if (this.items[i].quality > 0) {
+    //       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+    //         this.items[i].quality = this.items[i].quality - 1;
+    //       }
+    //     }
+    //   } else {
+    //     if (this.items[i].quality < 50) {
+    //       this.items[i].quality = this.items[i].quality + 1;
+    //       if (
+    //         this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+    //       ) {
+    //         if (this.items[i].sellIn < 11) {
+    //           if (this.items[i].quality < 50) {
+    //             this.items[i].quality = this.items[i].quality + 1;
+    //           }
+    //         }
+    //         if (this.items[i].sellIn < 6) {
+    //           if (this.items[i].quality < 50) {
+    //             this.items[i].quality = this.items[i].quality + 1;
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    //   if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+    //     this.items[i].sellIn = this.items[i].sellIn - 1;
+    //   }
+    //   if (this.items[i].sellIn < 0) {
+    //     if (this.items[i].name != "Aged Brie") {
+    //       if (
+    //         this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+    //       ) {
+    //         if (this.items[i].quality > 0) {
+    //           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+    //             this.items[i].quality = this.items[i].quality - 1;
+    //           }
+    //         }
+    //       } else {
+    //         this.items[i].quality =
+    //           this.items[i].quality - this.items[i].quality;
+    //       }
+    //     } else {
+    //       if (this.items[i].quality < 50) {
+    //         this.items[i].quality = this.items[i].quality + 1;
+    //       }
+    //     }
+    //   }
+    // }
+
+    //new code
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+      const itemService = GildRoseItemServiceFactory.getItemService(
+        this.items[i]
+      );
+      itemService.updateItemQuality(this.items[i]);
     }
 
     return this.items;
